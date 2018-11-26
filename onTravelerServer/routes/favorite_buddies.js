539b94a8f8706b486c4e901311a5491763399c45
hanhyeonmin <um4825@gmail.com>
2018-11-27 02:32:46 +0900

    favorite buddy rest api add

--- onTravelerServer/routes/favorite_buddies.js
+++ onTravelerServer/routes/favorite_buddies.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Favorite_buddy = require('../models/favorite_buddy');
+var Buddy = require('../models/buddy');
 var ReturnFormat = require('../return_form')();
 
 /* GET Favorite_buddy listing. */
@@ -16,6 +17,10 @@ router.get('/', function(req, res, next) {
 });
 
 
+
+
+
+
 /* Get particular favorite_buddies*/
 router.get('/customer/:customer_id', function(req, res, next){
     Favorite_buddy.find({customer_id : req.params.customer_id})
@@ -29,11 +34,36 @@ router.get('/customer/:customer_id', function(req, res, next){
 });
 
 
+router.get('/customer/:customer_id', function(req, res, next){
+    Favorite_buddy.find({customer_id : req.params.customer_id})
+        .then(function(favorite_buddy){
+            if(!favorite_buddy.length) return res.status(404).send(ReturnFormat.get_format(0, favorite_buddy, "not_found"));
+            res.send(ReturnFormat.get_format(1, favorite_buddy, ""));
+        })
+        .catch(function(err){
+            res.status(500).send(ReturnFormat.get_format(0, "", err));
+        });
+});
+
+
+
 /* Get particular favorite_buddies*/
-router.get('/buddy/:buddy_id', function(req, res, next){
+router.get('/buddies_info/:buddy_id', function(req, res, next){
     Favorite_buddy.find({buddy_id : req.params.buddy_id})
         .then(function(favorite_buddy){
             if(!favorite_buddy.length) return res.status(404).send(ReturnFormat.get_format(0, favorite_buddy, "not_found"));
+
+            var buddy_id_list = [];
+            for(var i=0; i<favorite_buddy.length; i++){
+                buddy_id_list[i] = favorite_buddy[i].buddy_id;
+            }
+
+            Buddy.find({buddy_id: {$in : buddy_id_list}})
+                .then(function (buddy) {
+                    if(!buddy.length) return res.status(404).send(ReturnFormat.get_format(0, buddy, "not_found"));
+                    res.send(ReturnFormat.get_format(1, buddy, ""));
+                })
+
             res.send(ReturnFormat.get_format(1, favorite_buddy, ""));
         })
         .catch(function(err){
